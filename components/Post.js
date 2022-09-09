8e45c24e83152a0a32ff91a967baa90d5645cf7f
William Robersone <wmrobersone@yahoo.com>
2022-09-08 21:13:53 -0500

    suggestions

--- components/Post.js
+++ components/Post.js
@@ -40,6 +40,15 @@ function Post({ id, username, userImg, img, caption }) {
       </p>
       {/* comments */}
       {/* input box */}
+      <form className="flex items-center p-4">
+        <EmojiHappyIcon className="h-7" />
+        <input
+          type="text"
+          placeholder="Add a comment..."
+          className="border-none flex-1 focus:ring-0 outline-none"
+        />
+        <button className="font-semibold text-blue-400">Post</button>
+      </form>
     </div>
   );
 }
